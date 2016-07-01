24d7adf94b8e52eb78b5665c2f4cd31afd03462b
thor <thorwhalen1@gmail.com>
2016-07-01 15:15:42 -0700

    point closest to centroid

--- stats/util.py
+++ stats/util.py
@@ -1,6 +1,6 @@
 __author__ = 'thor'
 
-from numpy import zeros, argmin, array
+from numpy import zeros, argmin, array, unique, where
 from scipy.spatial.distance import cdist
 
 
@@ -28,6 +28,17 @@ def normalize_to_one(arr):
     return arr / float(sum(arr))
 
 
+def point_idx_closest_to_centroid(X, centroid=None):
+    """
+    X is a n x m ndarray of n points in m dimensions.
+    point_closest_to_centroid(X, centroid) returns the index of the point of X (a row of X) that is closest to the given
+    centroid point. If centroid is not given, the actual centroid, X.mean(axis=1), is taken.
+    """
+    if centroid is None:
+        centroid = X.mean(axis=0)
+    return argmin(cdist(X, [centroid])[:, 0])
+
+
 def point_closest_to_centroid(X, centroid=None):
     """
     X is a n x m ndarray of n points in m dimensions.
@@ -39,5 +50,11 @@ def point_closest_to_centroid(X, centroid=None):
     return X[argmin(cdist(X, [centroid])[:, 0])]
 
 
-
+def get_cluster_representatives_indices(X, clus_labels, clus_centers):
+    representative_indices = list()
+    for label in unique(clus_labels):
+        cluster_point_indices = where(clus_labels == label)[0]
+        min_idx = argmin(cdist(X[cluster_point_indices, :], [clus_centers[label, :]])[:, 0])
+        representative_indices.append(cluster_point_indices[min_idx])
+    return array(representative_indices)
 
