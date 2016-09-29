33ae755a0dc19727b22a95469101a2a758281e6a
thor <thorwhalen1@gmail.com>
2016-09-28 17:10:27 -0700

    Moved s3io.py to s3io_old.py and replaced with new one:
    - updated the constructor to contain as argument the location also. REASON: for IATIS-EMUL the location is diferent ('us-east-1'). I added default value so there should be no impact on code already using the library
    - added save_file method to allow content to be saved directly from file, with dots for feedback on progress

--- ams3/s3io_old.py
+++ ams3/s3io_old.py
@@ -6,10 +6,7 @@ import boto.s3
 import tempfile
 import pickle
 import os
-import sys
 from ut.util.importing import get_environment_variable
-
-S3_DEFAULT_REGION = 'eu-west-1'
 
 
 class S3(object):
@@ -17,36 +14,20 @@ class S3(object):
     Storage to Amazon S3.
     """
 
-    def __init__(self, bucket_name, region=S3_DEFAULT_REGION):
+    def __init__(self, bucket_name):
         """
         Creates an instance with a handle on the S3 bucket corresponding to bucket_name. If bucket_name does not
         exist, it is created.
 
         :type bucket_name: str
         """
-        self.conn = boto.s3.connect_to_region(region,
+        self.conn = boto.s3.connect_to_region('eu-west-1',
                                               aws_access_key_id=get_environment_variable('VEN_S3_SECRET'),
                                               aws_secret_access_key=get_environment_variable('VEN_S3_ACCESS_KEY')
-                                              )
+        )
         if bucket_name not in [b.name for b in self.conn.get_all_buckets()]:
             self.conn.create_bucket(bucket_name, location=Location.EU)
         self.bucket = self.conn.get_bucket(bucket_name)
-
-    def save_file(self, key_name, file, folder=''):
-        """
-        Saves the key, file pair to S3 bucket and prints progress as dots
-
-        :param key: the key to save
-        :param value: the file to save
-        """
-
-        def percent_cb(complete, total):
-            sys.stdout.write('.')
-            sys.stdout.flush()
-
-        k = Key(self.bucket)
-        k.key = folder + key_name
-        k.set_contents_from_file(file, cb=percent_cb, num_cb=10)
 
     def save_object(self, key_name, value, folder=''):
         """
@@ -99,10 +80,10 @@ class S3(object):
         k = self.bucket.lookup(folder + key_name)
         return k.get_contents_to_string()
 
-    def download(self, key_name, filepath='', s3_folder='', local_folder='', suffix='', prefix=''):
+    def download(self, key_name, filepath='', s3_folder='',local_folder='',suffix='',prefix=''):
         if not filepath:
             filepath = key_name
-        filepath = local_folder + prefix + filepath + suffix
+        filepath = local_folder +  prefix + filepath + suffix
         print filepath
         if not os.path.exists(filepath):
             k = self.bucket.lookup(s3_folder + key_name)
