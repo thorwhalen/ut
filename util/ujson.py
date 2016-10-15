ba454ebfcda26ad48f3ccddf47df0c725721946f
thor <thorwhalen1@gmail.com>
2016-10-15 14:12:35 -0700

    added traceback in get_dict_or_error_object

--- util/ujson.py
+++ util/ujson.py
@@ -1,13 +1,16 @@
 from __future__ import division
 
 import json
+import traceback
 
 __author__ = 'thor'
 
 
+def get_dict_of_error_object(error_object, with_traceback=True):
 
-def get_dict_of_error_object(error_object):
-    return {'error': str(error_object.__class__), 'message': error_object.args[0]}
+    msg_dict = {'error': str(error_object.__class__), 'message': error_object.args[0]}
+    if with_traceback:
+        msg_dict['traceback'] = traceback.format_exc()
 
 
 def get_json_of_error_object(error_object):
