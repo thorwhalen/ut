6ef40a1984c31289b282c4fb8f3e9e13608d4ed9
thor <thor@otosense.com>
2017-03-23 16:06:46 -0700

    a newer version of feature balancing

--- ml/prep/feature_balancing.py
+++ ml/prep/feature_balancing.py
@@ -2,10 +2,61 @@ __author__ = 'thor'
 
 import pandas as pd
 from numpy import array, random
+from sklearn.cluster import KMeans
+from scipy.cluster.vq import vq
+from collections import Counter
+
+
+def balanced_data(X, y, method='sample'):
+    """
+    Get get subset of X (and aligned y) such that all y elements have the same count.
+    (i.e. all(Counter(y) == min(Counter(y).values())))
+
+    :param X: data matrix
+    :param y: label array (number aligned with rows of X)
+    :param method:
+        "sample" (will choose sample by random sampling without replacement)
+        "knn" (will choose the nearest neighbors of the centroids of each label subset -- much more representative,
+        but also a lot lot slower)
+
+    :return: A subset of X and aligned y
+    """
+    yc = Counter(y)
+    min_yc = min(yc.values())
+    new_X = list()
+    new_y = list()
+    for yi in yc.keys():
+        yi_lidx = y == yi
+        XX = X[yi_lidx, :]
+        yy = y[yi_lidx]
+        if yc[yi] != min_yc:
+            if method == 'knn':
+                km = KMeans(n_clusters=min_yc).fit(XX)
+                knn_idx, _ = vq(XX, km.cluster_centers_)
+            else:
+                knn_idx = random.choice(len(XX), min_yc, replace=False)
+            new_X.extend(XX[knn_idx, :].tolist())
+            new_y.extend(yy[knn_idx])
+        else:
+            new_X.extend(XX.tolist())
+            new_y.extend(yy.tolist())
+
+    return array(new_X), array(new_y)
 
 
 def label_balanced_subset(data, label, random_seed=None):
+    """
+    An newer version of this can be found in balanced_data.
 
+    Get get subset of X (and aligned y) such that all y elements have the same count.
+    (i.e. all(Counter(y) == min(Counter(y).values())))
+
+    :param data: data matrix or dataframe containing the data and the labels
+    :param label: label array (number aligned with rows of data) or name of the column where labels are in a dataframe
+        data input.
+    :param random_seed: seed for the random selection
+    :return: A subset of X and aligned y
+    """
     if not isinstance(data, pd.DataFrame) or label not in data.columns:
         # ... then assume data is the X and label is the y arrays of the supervised learning setup
         return_arrays = True
@@ -32,5 +83,3 @@ def label_balanced_subset(data, label, random_seed=None):
         return subset_data.as_matrix(), y
     else:
         return subset_data
-
-
