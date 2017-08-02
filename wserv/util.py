847324931ca0a9f433293dbb75a2c17b7b659258
thor <thor@otosense.com>
2017-08-02 12:52:52 -0700

    to_curl now accepts requests objects (will look to see if there's a 'request' attribute in the input. Also, headers defaults to '"Content-Type: application/json"

--- wserv/util.py
+++ wserv/util.py
@@ -1,14 +1,20 @@
 from __future__ import division
 
 
-def to_curl(request, headers=None):
+def to_curl(request, headers="Content-Type: application/json"):
     """
     Get a curl string from a python request.
     :param request: a requests.request object
     :return: a curl string corresponding to this request
     """
 
-    headers = headers or {k: v for k, v in request.headers.iteritems()}
+    if hasattr(request, 'request'):
+        request = getattr(request, 'request')
+
+    if headers is None:
+        headers = {k: v for k, v in request.headers.iteritems()}
+
+    # headers = headers or {k: v for k, v in request.headers.iteritems()}
 
     if isinstance(headers, dict):
         headers = ["'{}: {}'".format(k, v) for k, v in headers.iteritems()]
