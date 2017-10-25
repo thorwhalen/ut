e409f24866af99a7c4ea51ff74d61469808f9c45
thor <thor@otosense.com>
2017-10-25 07:53:31 -0700

    functions to (roughly) extract imports from a file and count them over all files under a root folder

--- util/resources.py
+++ util/resources.py
@@ -5,6 +5,24 @@ import os
 import sys
 import subprocess
 import inspect
+from collections import Counter
+from ut.pfile.to import string as file_to_str
+from ut.pfile.iter import get_filepath_iterator
+
+
+def extract_package_list_from_filepath(filepath):
+    s = file_to_str(filepath)
+    ss = s.split('\n')
+    import_capture_p = re.compile('(import|from)\s([^\.\s\n]+)')
+    return map(lambda x: x.group(2), filter(None, map(import_capture_p.match, ss)))
+
+
+def count_packages_of_all_py_files_recursively_under(root_folder):
+    c = Counter()
+    for filepath in get_filepath_iterator(root_folder, pattern='.py$'):
+        c.update(extract_package_list_from_filepath(filepath))
+    return c
+
 
 PIPFREEZE_LIST_FOLDER = '/D/Dropbox/dev/py/info/'
 PIPFREEZE_AIR = '/D/Dropbox/dev/py/info/pipfreeze_air_sem.txt'
@@ -20,6 +38,7 @@ def put_pip_freeze_output_into_file(output_file_path, environment=None):
         "pip freeze > {}".format(output_file_path))
     print s
 
+
 def import_subdirs_of(dir):
     subdirs = [x for x in get_immediate_subdirectories(dir) if x[0] != '.']
     for s in subdirs:
@@ -30,6 +49,7 @@ def import_subdirs_of(dir):
 def get_immediate_subdirectories(dir):
     return [name for name in os.listdir(dir)
             if os.path.isdir(os.path.join(dir, name))]
+
 
 # needs to be primed with an empty set for loaded (THIS IS SOMEONE ELSES CODE. NOT SURE IF IT WORKS
 def recursively_reload_all_submodules(module, loaded=None):
@@ -51,6 +71,7 @@ def mk_pip_freeze_list_file(computer_name, environment=None):
     s = os.system(
         "pip freeze > {}".format(PIPFREEZE_LIST_FOLDER + mk_list_file_name(computer_name, environment=environment)))
     print s
+
 
 def pipfreeze_list(filepath, unversioned=True):
     if filepath == 'all':
@@ -90,6 +111,7 @@ def missing_pip_installs(compare_set, unversioned=True):
     all_set = pipfreeze_list('all', unversioned=unversioned)
     return list(set(all_set) - set(compare_set))
 
+
 # def pipfreeze_air_minus_pro(unversioned=True):
 #     if unversioned==True:
 #         air = unversioned_pipfreeze_list(PIPFREEZE_AIR)
@@ -121,4 +143,3 @@ def pip_install_list(package_names, environment='sem'):
 def pip_install_missing(compare_set, environment='sem'):
     missing_packages = missing_pip_installs(compare_set, unversioned=True)
     pip_install_list(missing_packages, environment=environment)
-
