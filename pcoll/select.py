12297a92afb6ab6bfa1031d1d06a313ea4322ee3
thor <thor@otosense.com>
2017-12-23 15:29:28 -0500

    IATIS-2720: Added doc and doctest to sublist_that_contains_segment

--- pcoll/select.py
+++ pcoll/select.py
@@ -3,18 +3,47 @@ from __future__ import division
 inf = float('inf')
 
 
-def sublist_that_contains_segment(sorted_vals, from_val=None, to_val=None, key=None):
+def sublist_that_contains_segment(sorted_vals, from_val=-inf, to_val=inf, key=None):
     """
     Returns the sublist of a sorted iterator that tightly contains both from_val and to_val.
-     That is, it is the smallest sublist containing the [from_val, to_val] interval.
+     In some sense, it is the smallest sublist containing the [from_val, to_val] interval.
+     We say "in some sense" because the function will range from the largest value strictly smaller than from_val to
+     the smallest value strictly greater than to_val.
+     For example, if sorted_vals = [0, 5, 10, 15, 20, 25, 30], from_val=10, and to_val=20, the function will return
+        [5, 10, 15, 20, 25]
+     as opposed to
+        [10, 15, 20, 25]
+    This is useful when the sorted_vals are actually taken from a larger sorted list and you want to make sure that
+    the interval returned contains ALL vals that are greater OR EQUAL to from_val and smaller OR EQUAL to to_val.
+    We call this the "superset-safe" property.
     :param sorted_vals: A sorted iterator
-    :param from_val: The smallest value to contain
-    :param to_val: The largest value to contain
+    :param from_val: The smallest value to contain. If None will take from_val=-inf
+    :param to_val: The largest value to contain. If None will take from_val=inf
     :param key: How to get the vals (to compare with) from the sorted_vals iterator elements.
         * None: Will just take the element itself
         * callable: Will take key(element) as the val
         * else will take element[key] as the val (hoping that key is a hashable, and exists as a key to element)
     :return: A list of the elements of sorted_vals.
+    >>> sorted_vals = [0, 5, 10, 15, 20, 25, 30]
+    >>> sublist_that_contains_segment(sorted_vals, 11, 21)
+    [10, 15, 20, 25]
+    >>> # test the superset-safe property (see description above)
+    >>> sublist_that_contains_segment(sorted_vals, 10, 20)
+    [5, 10, 15, 20, 25]
+    >>> # test specifying only from_val
+    >>> sublist_that_contains_segment(sorted_vals, from_val=11)
+    [10, 15, 20, 25, 30]
+    >>> # test specifying only to_val
+    >>> sublist_that_contains_segment(sorted_vals, to_val=21)
+    [0, 5, 10, 15, 20, 25]
+    >>> # test a functional key
+    >>> sorted_vals = [{'a': 1, 'b': 2}, {'a': 2, 'b': 3}, {'a': 3, 'b': 4}, {'a': 4, 'b': 5}, {'a': 5, 'b': 6}]
+    >>> sublist_that_contains_segment(sorted_vals, 7, 12, key=lambda x: x['a'] * x['b'])
+    [{'a': 2, 'b': 3}, {'a': 3, 'b': 4}, {'a': 4, 'b': 5}]
+    >>> # test a hashable key
+    >>> sorted_vals = [{'a': 1, 'b': 2}, {'a': 2, 'b': 3}, {'a': 3, 'b': 4}, {'a': 4, 'b': 5}, {'a': 5, 'b': 6}]
+    >>> sublist_that_contains_segment(sorted_vals, 2.1, 3, key='a')
+    [{'a': 2, 'b': 3}, {'a': 3, 'b': 4}, {'a': 4, 'b': 5}]
     """
     sublist = list()
     if from_val is None:
