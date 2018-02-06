547910c6a088511e338cc3a88f215d72636cd5e7
thor <thor@otosense.com>
2018-02-06 07:49:39 -0800

    lite_pandas

--- daf/lite_pandas.py
+++ daf/lite_pandas.py
@@ -1,23 +1,20 @@
 from __future__ import division
 
-import types
 from collections import defaultdict
-import pandas as pd
 import numpy as np
 
-pd.DataFrame
 
-class DataFrame(object):
+class MatrixDataFrame(object):
     def __init__(self, data=None, index=None, columns=None):
         if data is None:
             data = {}
 
-        if isinstance(data, DataFrame):
+        if isinstance(data, MatrixDataFrame):
             data = data._d
 
         if isinstance(data, dict):
             self._d = np.hstack(data.values())
-        elif isinstance(data, (np.ndarray, list)):
+        elif isinstance(data, (np.ndarray, list, np.matrix)):
             n = len(data)
             if n == 0:
                 self._d = np.array([])
@@ -28,6 +25,8 @@ class DataFrame(object):
                         for k, v in d.iteritems():
                             col_data[k].append(v)
                     self._d = np.array(col_data)
+                else:
+                    self._d = np.array(data)
 
         if index is None:
             index = np.arange(self._d.shape[0])
@@ -37,3 +36,50 @@ class DataFrame(object):
         self.index = np.array(index)
         self.columns = np.array(columns)
 
+    def sum(self, axis=0):
+        if axis == 0:
+            data = self._d.sum(axis=axis)
+            return MatrixDataFrame(data=data, columns=self.columns)
+        elif axis == 1:
+            raise NotImplementedError("not implemented yet")
+
+    def __str__(self):
+        s = ""
+        s += "{}\n".format(self.columns)
+        s += "{}".format(self._d)
+        return s
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class RecDataFrame(object):
+    def __init__(self, data=None, index=None, columns=None):
+        if isinstance(data, np.recarray):
+            self._d = data
+        elif isinstance(data, (list, np.ndarray, np.matrix)):
+            if columns is not None:
+                self._d = np.rec.fromarrays(data, names=columns)
+            else:
+                self._d = np.rec.fromarrays(data)
+        else:
+            raise TypeError("Don't know how to construct a RecDataFrame with that input type")
+
+        if index is None:
+            index = np.arange(self._d.shape[0])
+        self.index = index
+
+    @property
+    def columns(self):
+        return np.array(self._d.dtype.names)
+
+    def __str__(self):
+        return self._d.__str__()
+
+    def __repr__(self):
+        return self._d.__repr__()
+
+
+class DictDataFrame(object):
+    def __init__(self, data=None, index=None, columns=None):
+        pass
