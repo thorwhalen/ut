57c6dca84b2b3d919e49dfed068dcc72c928c89f
twhalen <thor@otosense.com>
2020-04-30 12:28:34 -0400

    import_from_dot_string

--- util/importing.py
+++ util/importing.py
@@ -38,3 +38,37 @@ def module_import_from_string(import_path_string, params_file):
         p = importlib.import_module(params_module)
 
     return p
+
+
+import importlib
+
+
+def import_from_dot_string(dot_string):
+    """
+    Import an object from a dot string path to the object.
+    :param dot_string: The dot-path to the module
+    :return: The desired object
+
+    >>> import os
+    >>> module = type(os)
+    >>> # can import modules
+    >>> assert import_from_dot_string('os') == os
+    >>> assert import_from_dot_string('os.path') == os.path
+    >>> assert isinstance(os.path, module)
+    >>> # can import strings
+    >>> assert import_from_dot_string('os.path.sep') == os.path.sep
+    >>> assert isinstance(os.path.sep, str)
+    >>> # can import callables
+    >>> assert import_from_dot_string('os.path.join') == os.path.join
+    >>> assert callable(os.path.join)
+    >>> # basically, can import objects
+    """
+    dot_list = dot_string.split('.')
+    if len(dot_list) > 1:
+        prefix = '.'.join(dot_list[:-1])
+        suffix = dot_list[-1]
+        module = importlib.import_module(prefix)
+        obj = getattr(module, suffix)
+        return obj
+    else:
+        return importlib.import_module(dot_string)
