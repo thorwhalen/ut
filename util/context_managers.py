bcf914868fcaff891b4466614ad23467bb59d2ff
Thor Whalen <thorwhalen1@gmail.com>
2020-12-02 13:47:22 -0800

    AP's cd context manager

--- util/context_managers.py
+++ util/context_managers.py
@@ -1,6 +1,26 @@
 from warnings import warn
+from ut import ModuleNotFoundIgnore
 
-warn("This module moved to the lag package (pip install lag). I'll import from there (if you have it).",
-     DeprecationWarning)
+with ModuleNotFoundIgnore():
+    from lag import *
 
-from lag import *
+import os
+import contextlib
+
+
+@contextlib.contextmanager
+def cd(newdir, verbose=True):
+    """Change your working directory, run the function, and change back to the original"""
+    prevdir = os.getcwd()
+    os.chdir(os.path.expanduser(newdir))
+    try:
+        if verbose: print(f'cd {newdir}')
+        yield
+    finally:
+        if verbose: print(f'cd {prevdir}')
+        os.chdir(prevdir)
+    from pathlib import Path
+    if verbose: print("Called before cd", Path().absolute())
+    with cd(Path.home()):
+        if verbose: print("Called under cd", Path().absolute())
+    if verbose: print("Called after cd and same as before", Path().absolute())
