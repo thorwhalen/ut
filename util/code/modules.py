648f3227b42b5990cfbbad61516146a157ad9187
Thor Whalen <thorwhalen1@gmail.com>
2020-12-16 11:14:57 -0800

    more on module exploration

--- util/code/modules.py
+++ util/code/modules.py
@@ -5,8 +5,10 @@ import importlib
 import importlib.util
 from dataclasses import dataclass
 import ast
+import site
 from enum import Enum
 from collections import namedtuple
+import inspect
 
 
 def is_from_module(obj, module):
@@ -93,27 +95,46 @@ def loaded_module_from_dotpath_and_filepath(dotpath, filepath):
     return module
 
 
-def submodules(module, prefix=None):
+def module_path(module):
+    try:
+        _module_path = module.__path__[0]
+    except:
+        _module_path = inspect.getfile(module)
+        if _module_path.endswith('__init__.py'):
+            _module_path = module_path[:len('__init__.py')]
+    return _module_path
+
+
+def submodules(module, on_error='print', prefix=None):
     from py2store.filesys import FileCollection
 
     prefix = prefix or site.getsitepackages()[0]
 
-    for filepath in FileCollection(module.__path__[0], '{f}.py', max_levels=0):
+    _module_path = module_path(module)
+    assert os.path.isdir(_module_path), f"Should be a directory: {module_path}"
+
+    for filepath in FileCollection(_module_path, '{f}.py', max_levels=0):
         dotpath = '.'.join(filepath[len(prefix):(-len('.py'))].split(os.path.sep))
+        if dotpath.startswith('.'):
+            dotpath = dotpath[1:]
         if not dotpath.endswith('__init__'):
             try:
                 yield loaded_module_from_dotpath_and_filepath(dotpath, filepath)
-            except ModuleNotFoundError as e:
-                print(e)
+            except Exception as e:
+                if on_error == 'print':
+                    print(f"{e} ({dotpath}: {filepath})")
+                elif on_error == 'raise':
+                    raise
 
 
 def objects_of_module(module, max_levels=0):
     for a in dir(module):
         if not a.startswith('_'):
             yield getattr(module, a)
-    if max_levels > 0:
-        for submodule in submodules(module):
-            yield from objects_of_module(submodule, max_levels - 1)
+    if os.path.isdir(module_path(module)):
+        if max_levels > 0:
+            for submodule in submodules(module):
+                yield from objects_of_module(submodule, max_levels - 1)
 
 
 def obj_to_dotpath(obj):
