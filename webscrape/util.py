353a4629c35a2cca76ef91a4d5209afe766433b4
Thor Whalen <thorwhalen1@gmail.com>
2021-12-09 15:00:58 -0800

    feat: html2text contextualizing it's definition

--- webscrape/util.py
+++ webscrape/util.py
@@ -1,19 +1,26 @@
+"""Webscraping utils"""
+
 __author__ = 'thor'
 
-from pattern.web import plaintext
-# from ut.parse import html2text_formated
+from contextlib import suppress
 import os
 import re
-from bs4 import BeautifulSoup
-import requests
 
 params = dict()
 params['html_ext_re'] = re.compile('.html$')
 
+ignore_module_error = suppress(ModuleNotFoundError, ImportError)
+
 
 def html2text(text):
-    return plaintext(text)
-    # return html2text_formated.html2text(text).replace('**', '')
+    with ignore_module_error:
+        from pattern.web import plaintext as html2text
+
+        return plaintext(text)
+    with ignore_module_error:
+        from html2text import html2text
+
+        return html2text(text)
 
 
 def filename_from_url(url, rm_http=True):
@@ -23,12 +30,32 @@ def filename_from_url(url, rm_http=True):
 
 
 def url_from_filename(filename, prefix=''):
-    return prefix + params['html_ext_re'].sub('', os.path.basename(filename).replace('~', '/').replace('{', ':'))
+    return prefix + params['html_ext_re'].sub(
+        '', os.path.basename(filename).replace('~', '/').replace('{', ':')
+    )
 
 
-def what_ip_do_they_see(get_html_fun=lambda url: requests.get(url).text):
-    b = BeautifulSoup(get_html_fun('http://www.whatismyip.com/'))
-    return b.find('div', 'the-ip').text
+with ignore_module_error:
+    from bs4 import BeautifulSoup
+    import requests
+
+    def what_ip_do_they_see(get_html_fun=lambda url: requests.get(url).text):
+        b = BeautifulSoup(get_html_fun('http://www.whatismyip.com/'))
+        return b.find('div', 'the-ip').text
+
+    def all_links_of_html(html):
+        from bs4 import BeautifulSoup
+
+        t = BeautifulSoup(html, features='lxml')
+        return list(
+            dict.fromkeys(  # this removes duplicates conserving order
+                x.get('href')
+                for x in t.find_all(
+                    attrs={'href': re.compile('http.*')}, recursive=True
+                )
+            )
+        )
+
 
 # def filename_from_url_httpless(url)
 
